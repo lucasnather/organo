3f1987ad52651c654f5b4ec9fab9a012a0bfff04
lucasnather <natherlucas@gmail.com>
2024-06-19 15:17:11 -0400

    feat: schema and register fields form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,28 +1,51 @@
+import { z } from "zod";
 import { Button, ContainerInputs, FormContainer, Input, Label, Legend, Select } from "./style";
+import { useForm } from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formCardSchema = z.object({
+    name: z.string().min(3, "Mínimno de 3 caracters"),
+    role: z.string(),
+    image: z.string().url("O formato precisa estar em URL"),
+    team: z.string()
+})
+
+type FormCardType = z.infer<typeof formCardSchema>
 
 export function Form() {
+    const { register, reset, handleSubmit } = useForm<FormCardType>({
+        resolver: zodResolver(formCardSchema)
+    })
+
+    function handleSubmitForm(data: FormCardType) {
+        console.log(data)
+        reset()
+    }
+
     return (
-        <FormContainer action="#">
+        <FormContainer action="#" onSubmit={handleSubmit(handleSubmitForm)}>
             <Legend>Preencha os dados para criar o card do colaborador.</Legend>
 
             <ContainerInputs>
                 <Label htmlFor="name">Nome</Label>
-                <Input type="text" name="name" id="name" placeholder="Digite seu nome"/>
+                <Input type="text" {...register('name')} placeholder="Digite seu nome"/>
             </ContainerInputs>
 
             <ContainerInputs>
                 <Label htmlFor="role">Cargo</Label>
-                <Input type="text" name="role" id="role" placeholder="Digite seu cargo" />
+                <Input type="text" {...register('role')} placeholder="Digite seu cargo" />
             </ContainerInputs>
 
             <ContainerInputs>
                 <Label htmlFor="image">Imagem</Label>
-                <Input type="text" name="image" id="image" placeholder="Informe o endereço da imagem" />
+                <Input type="text" {...register('image')} placeholder="Informe o endereço da imagem" />
             </ContainerInputs>
 
             <ContainerInputs>
                 <Label htmlFor="team">Time</Label>
-                <Select name="team" id="team"></Select>
+                <Select {...register('team')}>
+                    <option value="am">am</option>
+                </Select>
             </ContainerInputs>
             
             <Button type="submit">Criar Card</Button>
